38a4dab17f7bdd20b76d60d94fed26e0df61530c
celineanabusi <cileneanabusi@campus.technion.ac.il>
2026-01-06 10:43:13 +0200

    Add bumpbypriorty function

--- TaskManager.cpp
+++ TaskManager.cpp
@@ -17,3 +17,24 @@ void TaskManager::completeTask(const string &personName) {
     if (person_num != -1)
         workers[person_num].completeTask();
 }
+
+void TaskManager::bumpPriorityByType(TaskType type, int priority) {
+    if (priority < 0) {
+        return;
+    }
+    SortedList<Task> new_tasks;
+    for (int i = 0; i < num_of_workers; i++) {
+        const SortedList<Task> &tasks = workers[i].getTasks();
+        for (SortedList<Task>::ConstIterator it = tasks.begin(); it != tasks.end(); ++it) {
+            Task curr_task = (*it);
+            if (curr_task.getType() == type) {
+                Task new_task(curr_task.getPriority() + priority, curr_task.getType(), curr_task.getDescription());
+                new_task.setId(curr_task.getId());
+                new_tasks.insert(new_task);
+            } else {
+                new_tasks.insert(curr_task);
+            }
+        }
+        workers[i].setTasks(new_tasks);
+    }
+}
